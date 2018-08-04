a23168d8ba1e06373dae4a89b47773503d636bd5
Abel Trotter <a_trotter0@hotmail.com>
2018-08-04 12:45:31 -0700

    Adjust tests to match specs

--- spec/galactic-calculator-spec.js
+++ spec/galactic-calculator-spec.js
@@ -4,17 +4,17 @@ describe('GalacticCalculator', function() {
   let calculator;
 
   beforeEach(function() {
-    calculator = new GalacticCalculator(33, "1/1/1984");
+    calculator = new GalacticCalculator(32, "1/1/1985");
   });
 
   it('should test that the object is instantiated with the correct properties', function() {
-    expect(calculator.ageInput).toEqual(33);
-    expect(calculator.dobInput).toEqual("1/1/1984");
+    expect(calculator.ageInput).toEqual(32);
+    expect(calculator.dobInput).toEqual("1/1/1985");
     expect(calculator.lifeExpectancy).toEqual(78);
   });
 
   it('should test that a users age is converted to seconds', function() {
-    const expectedSeconds = 33 * 31536000;
+    const expectedSeconds = 32 * 31536000;
     expect(calculator.getAgeInSeconds()).toEqual(expectedSeconds);
   });
 });
